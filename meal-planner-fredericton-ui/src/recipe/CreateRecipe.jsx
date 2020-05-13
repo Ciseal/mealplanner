9423f62f3218a364fc3796fa915cb247c268e1a5
Sam MacPhee <sammacphee@Sams-MacBook-Pro.local>
2020-05-13 20:54:59 -0300

    CreateRecipe

--- meal-planner-fredericton-ui/src/recipe/CreateRecipe.jsx
+++ meal-planner-fredericton-ui/src/recipe/CreateRecipe.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import Header from "../core/header/Header";
 import Footer from "../core/footer/Footer";
 import Container from "../components/container/Container";
-
+import { useFormik } from 'formik';
+import TextField from "@material-ui/core/TextField";
 
 const CreateRecipe = (props) => {
-
+const formik = useFormik({
+    initialValues: {
+      firstName: ''
+    },
+    onSubmit: values => {
+      console.log(values, "values");
+    },
+  });
     return (
         <>
             <Header />
                <Container>
+                <form onSubmit={formik.handleSubmit}>
+                 <TextField
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values.firstName}
+                 />
+                </form>
                </Container>
             <Footer />
         </>
